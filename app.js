703212491303f987c9db64e9d23d79f2ec65af9b
EmadEddin <aimad.tchad@gmail.com>
2018-01-20 04:57:42 +0000

    add and update fucntion are done also with express the delete function go well with ajax so it done too next steps fixing the messages handling which a already start with using connect-flash and express-message

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+
 
 mongoose.connect('mongodb://localhost/mystore', { useMongoClient: true });
 let db = mongoose.connection;
@@ -31,6 +36,37 @@ app.use(bodyParser.json());
 //set public folder (static files)
 app.use(express.static(path.join(__dirname,'public')));
 
+//Middleware Express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}))
+
+//Express messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Express Validator middleware
+//app.use(expressValidator(middlewareOptions));
+app.use(expressValidator({
+	errorFormatter : function(param,msg,value){
+		var namespace = param.split('.')
+		, root = namespace.shift()
+		, formParam = root;
+		while(namespace.length){
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return{
+			param : formParam,
+			msg : msg,
+			value : value
+		};
+	}
+}));
 
 //bring in models
 let Article = require('./models/articles');
@@ -86,13 +122,64 @@ app.post('/articles/add',(req , res) => {
 
 	article.save((err) =>{
 		if(err){
+			req.flash('danger','Article Probleme');
 			console.log(err);
 			//return;
 		}else{
+			req.flash('success','Article Added');
 			res.redirect('/');
 		}
 	})
 })
+
+
+
+//Load edit article
+app.get('/article/edit/:id',(req,res) =>{
+	Article.findById(req.params.id,(err,article) => {
+		if(err){
+			console.log(err);
+		}else{
+			req.flash("success","Article Added");
+			res.render('edit_article',{
+				title : 'Edit Article',
+				article : article
+			});
+		}
+	});
+});
+
+//update submit post
+app.post('/articles/edit/:id',(req , res) => {
+	let article = {};
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+
+	let query = {_id:req.params.id}
+	//Article is model not object acticle
+	Article.update(query,article,(err) =>{
+		if(err){
+			console.log(err);
+			//return;
+		}else{
+			req.flash('success','Article Added');
+			res.redirect('/');
+		}
+	});
+});
+
+app.delete('/article/:id',(req,res)=>{
+	let query = {_id:req.params.id}
+	Article.remove(query,(err)=>{
+		if(err){
+			console.log(err);
+		}else{
+			res.send('Delete Success');
+		}
+		
+	});
+});
 
 //set local server to port 3001
 app.listen(3001, () => {
---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function() {
+	$('.delete-article').on('click',function(e){
+		// $target = $(e.target);
+		// console.log($target.attr('data-id'));
+		const id = $(this).data('id');
+		$.ajax({
+			type: 'DELETE',
+			url: '/article/'+id,
+			success : (response)=>{
+				alert('Deleting Article');
+				window.location.href='/';
+			},
+			error : (err)=>{
+				console.log(err);
+			}
+		});
+	});
+});
